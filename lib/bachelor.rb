c6ba9a4207302f5a3f22e5828bba9e6cd4775a8b
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-17 02:52:25 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,85 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  array = []
+  data.each do |season_info, contestant_data|
+    if season_info ===season
+      contestant_data.each do |attributes|
+      attributes.each do |element, value|
+       if element == "status" && value == "Winner"
+          array.push(attributes["name"].split(" ")[0])
+          end
+       end
+    end
+  end
 end
+return array[0]
+end
+
 
 def get_contestant_name(data, occupation)
   # code here
+ array = []
+  data.each do |season_info, contestant_data|
+     contestant_data.each do |attributes|
+       attributes.each do |elements, value|
+         if elements == "occupation" && value == occupation
+            array.push(attributes["name"])
+          end
+
+       end
+    end
+  end
+return array[0]
 end
 
+
 def count_contestants_by_hometown(data, hometown)
+  array = []
+  data.each do |season_info, contestant_data|
+    contestant_data.each do |attributes|
+      attributes.each do |elements, value|
+      if elements === "hometown" && value == hometown
+        array.push(attributes["name"])
   # code here
-end
+        end
+      end
+    end 
+  end 
+  return array.length 
+end 
+
 
 def get_occupation(data, hometown)
   # code here
+    array = []
+  data.each do |season_info, contestant_data|
+    contestant_data.each do |attributes|
+      attributes.each do |elements, value|
+         if elements === "hometown" && value == hometown
+        array.push(attributes["occupation"])
+       end
+      end
+    end 
+  end 
+  return array[0] 
 end
 
+
 def get_average_age_for_season(data, season)
-  # code here
+    # code here
+  array = []
+  data.each do |season_info, contestant_data|
+    if season_info == season
+     contestant_data.each do |attributes|
+       attributes.each do |elements, value|
+         if elements == "age"
+            array.push(value.to_i)
+          end
+
+        end
+    end
+  end
 end
+return array.reduce(:+).fdiv(array.size).to_i 
+end
+
