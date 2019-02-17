4977aaadc8dbf6ef74657a5c615ff648dd782bc0
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-17 02:55:05 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -80,7 +80,6 @@ def get_average_age_for_season(data, season)
     end
   end
 end
-return mean = (array.sum / array.length)
-# return array.reduce(:+).fdiv(array.size).to_i 
+return array.reduce(:+).fdiv(array.size).to_i 
 end
 
